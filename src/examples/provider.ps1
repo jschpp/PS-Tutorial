d9ed0b056fc387aae76273aa9cc994cc427f5c73
Johannes Schöpp <mail@jschpp.de>
2017-12-27 15:56:58 +0100

    [WIP] Provider Examples

--- src/examples/provider.ps1
+++ src/examples/provider.ps1
@@ -1,2 +1,4 @@
-Get-PSProvider
-Set-Location HKLM:
+# TODO: Test this in windows and add informative Stuff
+Get-PSProvider # Zeigt Verfügbare Provider
+Set-Location HKCU: # Wechselt in die HKCU Registry
+Get-ChildItem . # Zeigt die 
